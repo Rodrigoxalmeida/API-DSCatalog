618d91f29054690ccd56ab54ade3890823d508a7
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-09-03 23:02:35 -0300

    Database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into tb_category(NAME) values ('Books');
+insert into tb_category(NAME) values ('Eletronics');
+insert into tb_category(NAME) values ('Computers');
+insert into tb_category(NAME) values ('Consoles');
+insert into tb_category(NAME) values ('Tvs');
+insert into tb_category(NAME) values ('CPU');
