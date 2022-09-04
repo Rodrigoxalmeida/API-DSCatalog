003b7a464643fe18009c08ad0ea584768f27191a
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-09-04 00:05:23 -0300

    DTO-Category

--- src/main/java/br/com/dscatalog/services/CategoryService.java
+++ src/main/java/br/com/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package br.com.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.dscatalog.dto.CategoryDTO;
 import br.com.dscatalog.entities.Category;
 import br.com.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());				
 	}
 	
 }
