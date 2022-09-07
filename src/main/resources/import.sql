52fc50ad715b52fb94e09d2a7f62b5b0c759f12b
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-09-07 19:03:50 -0300

    find category by id, exception handler

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,4 @@
 insert into tb_category(NAME) values ('Books');
 insert into tb_category(NAME) values ('Eletronics');
 insert into tb_category(NAME) values ('Computers');
-insert into tb_category(NAME) values ('Consoles');
-insert into tb_category(NAME) values ('Tvs');
-insert into tb_category(NAME) values ('CPU');
+
