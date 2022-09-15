a2f63b338b371ff39920d723264879ff2cff7854
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-09-14 22:05:59 -0300

    Auditing data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into tb_category(NAME) values ('Books');
-insert into tb_category(NAME) values ('Eletronics');
-insert into tb_category(NAME) values ('Computers');
+insert into tb_category(NAME, created_AT) values ('Books', NOW());
+insert into tb_category(NAME, created_AT) values ('Eletronics',NOW());
+insert into tb_category(NAME, created_AT) values ('Computers',NOW());
 
